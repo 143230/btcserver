4a113252b4a9aa6744990ff60644830110dc41f5
cuixuan <cuixuan@baidu.com>
2017-10-22 23:01:06 +0800

    add ")" for FileController.java:[112,48] ')' expected

--- src/main/java/com/btc/app/controller/FileController.java
+++ src/main/java/com/btc/app/controller/FileController.java
@@ -109,7 +109,7 @@ public class FileController {
     private void versionCheck() throws IOException {
         final String filepath = "/root/tomcat_files/version.conf";
         Properties prop = new Properties();
-        prop.load(new FileInputStream(filepath);
+        prop.load(new FileInputStream(filepath));
         ios_version = prop.getProperty("ios_version");
         //new String(props.getProperty(key).getBytes("ISO8859-1"), encode);
         ios_version_desc = new String(prop.getProperty("ios_version_desc").getBytes("ISO8859-1"), "UTF-8");
